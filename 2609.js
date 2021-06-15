9ab6be164f8021d9e12655f14da0148a00366fa3
Woo Jieum <oozeume@gmail.com>
2021-06-16 00:58:08 +0900

    Create 2609.js

---
+++ 2609.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const input = (
+    process.platform === 'linux'
+        ? fs.readFileSync('/dev/stdin').toString()
+        : // 예제 입력부분 (예제와 똑같이 입력!)
+        `24 18`
+).split(' ');
+
+let Num1 = Number(input[0]);
+let Num2 = Number(input[1]);
+
+let smallNum;
+let bigNum;
+
+if (Num2 > Num1) {
+    smallNum = Num1;
+    bigNum = Num2;
+} else {
+    smallNum = Num2;
+    bigNum = Num1;
+}
+
+while (bigNum % smallNum !== 0) {
+    let rest = bigNum % smallNum;
+    if (rest !== 0) {
+        bigNum = smallNum;
+        smallNum = rest;
+    }
+}
+
+console.log(smallNum);
+console.log(Num1*Num2 / smallNum);
