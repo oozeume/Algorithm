97ee033efa0b712bdc00d00b1efe2d11e5ae0790
Woo Jieum <oozeume@gmail.com>
2021-06-19 13:18:32 +0900

    BAEKJOON

---
+++ BAEKJOON/11050_이항계수_1.js
@@ -0,0 +1,23 @@
+// 팩토리얼 구하는 함수를 만들어 놓고
+// 팩토리얼 : 1부터 어떤 양의 정수 n까지의 정수를 모두 곱한 것 
+// 이항계수 공식을 사용해서 출력
+
+const fs = require('fs');
+const input = (
+    process.platform === 'linux'
+        ? fs.readFileSync('/dev/stdin').toString()
+        : // 예제 입력부분 (예제와 똑같이 입력!)
+        `5 2`
+).split(' ').map(Number);
+
+function factorial(num) {
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
+console.log(factorial(input[0]) / (factorial(input[1]) * factorial(input[0] - input[1])));
+
+
