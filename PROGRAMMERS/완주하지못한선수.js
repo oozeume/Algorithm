3eae5e5f2dfea0c8fe2ccd39c661a49bf8175cdd
Woo Jieum <oozeume@gmail.com>
2021-06-25 00:20:36 +0900

    PROGRAMMERS

---
+++ PROGRAMMERS/완주하지못한선수.js
@@ -0,0 +1,15 @@
+// function solution(participant, completion) {
+//     var answer = '';
+
+
+//     return answer;
+// }
+
+
+let participant = ["leo", "kiki", "eden"];
+let completion = ["eden", "kiki"];
+
+participant.sort();
+completion.sort();
+
+console.log(participant, completion);
