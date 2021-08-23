4ea2b936927f9e4c8ef004511784cb03015eb384
Woo Jieum <oozeume@gmail.com>
2021-08-24 02:47:14 +0900

    0823

---
+++ PROGRAMMERS/문자열다루기기본.js
@@ -0,0 +1,21 @@
+const solution = (s) => {
+  let answer = true;
+  let numArr = "1234567890";
+
+  // 문자열 하나씩 돌면서 숫자가 아니면 false반환, 모두 숫자이면 true반환
+  if (s.length === 4 || s.length === 6) {
+    for (let i = 0; i < s.length; i++) {
+      if (numArr.includes(s[i]) === true) {
+        continue;
+      } else {
+        return answer = false;
+      }
+    }
+  } else {
+    return answer = false;
+  }
+  return answer;
+}
+
+const s = "1234";
+console.log(solution(s));
