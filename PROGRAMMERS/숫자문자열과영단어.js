6d9fe9f77f73c227c248fa4dd8316111a9b2a3e4
Woo Jieum <oozeume@gmail.com>
2021-09-18 23:09:34 +0900

    0918

---
+++ PROGRAMMERS/숫자문자열과영단어.js
@@ -0,0 +1,13 @@
+const solution = (s) => {
+  let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+  for (let i = 0; i < numbers.length; i++) {
+    let arr = s.split(numbers[i]);
+    s = arr.join(i);
+  }
+
+  return Number(s);
+}
+
+const s = "22222three45sixseven";
+console.log(solution(s));
