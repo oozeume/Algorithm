e301f62cb2068785bad42e5cfcb0c77c7dc01506
Woo Jieum <oozeume@gmail.com>
2021-09-23 16:31:58 +0900

    0923

--- PROGRAMMERS/2주차_상호평가.js
+++ PROGRAMMERS/2주차_상호평가.js
@@ -25,24 +25,23 @@ const solution = (scores) => {
       let findIdx = scoreArr.indexOf(scoreArr[i]);
       let findCurrentIdx = scoreArr[i].indexOf(scoreArr[i][j]);
 
-      if (findIdx === findCurrentIdx) { // 각 행에서 내가 준 나의 점수 확인하기
-        // 배열 중에서 내가 준 내 점수가 유일하게 가장 작은 값이거나, 가장 큰 값이거나
+      if (findIdx === findCurrentIdx) {
 
-        const myScore = scoreArr[i][j]; // 내 점수는 잘 찾고있음
-        const dubCheck = isDuplicate(scoreArr[i]); // 중복 유무 확인
+        const myScore = scoreArr[i][j];
+        const dubCheck = isDuplicate(scoreArr[i]);
 
-        if (dubCheck) { // 내 점수와 중복된 값이 있으면
+        if (dubCheck) {
           if (Math.max(...scoreArr[i]) === myScore || Math.min(...scoreArr[i]) === myScore) {
             let myIdx = scoreArr[i].indexOf(myScore);
             console.log(myIdx);
-            scoreArr[i].splice(myIdx, 1, 0); // 잘못빠지고 있었네? 왜그렇지??
+            scoreArr[i].splice(myIdx, 1, 0);
             len--;
           } else {
-            sum += scoreArr[i][j]; // 그대로 진행
+            sum += scoreArr[i][j];
           }
         } else if (Math.max(...scoreArr[i]) === myScore || Math.min(...scoreArr[i]) === myScore) {
           let myIdx = scoreArr[i].indexOf(myScore);
-          scoreArr[i].splice(myIdx, 1, 0); // 잘못빠지고 있었네? 왜그렇지??
+          scoreArr[i].splice(myIdx, 1, 0);
           len--;
         } else {
           sum += scoreArr[i][j]
@@ -51,19 +50,19 @@ const solution = (scores) => {
         sum += scoreArr[i][j];
       }
     }
-    console.log(len); // 여기서 length가 제대로 안빠지고 있다. 
+    console.log(len);
     sumArr.push(sum / len);
   }
 
   // 학점 계산
   for (let i = 0; i < sumArr.length; i++) {
-    if (sumArr[i] >= Number(90)) {
+    if (sumArr[i] >= 90) {
       answer += 'A';
-    } else if (Number(90) > sumArr[i] && sumArr[i] >= Number(80)) {
+    } else if (90 > sumArr[i] && sumArr[i] >= 80) {
       answer += 'B';
-    } else if (Number(80) > sumArr[i] && sumArr[i] >= Number(70)) {
+    } else if (80 > sumArr[i] && sumArr[i] >= 70) {
       answer += 'C';
-    } else if (Number(70) > sumArr[i] && sumArr[i] >= Number(50)) {
+    } else if (70 > sumArr[i] && sumArr[i] >= 50) {
       answer += 'D';
     } else {
       answer += 'F';
